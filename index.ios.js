241f4d03aee8a1ac5f08073cc7a361cd4a25706a
Thiago Galvani <thiagopaiva99@gmail.com>
2017-10-01 02:35:08 -0300

    Modificando o arquivos de codigo do iOS

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,115 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, Text, View, Button, Image, StatusBar } from 'react-native'
 
 export default class jokenpo extends Component {
+
+  constructor( props ){
+    super( props )
+
+    this.state = {
+      userChoise: '',
+      computerChoise: '',
+      result: ''
+    }
+  }
+
+  jokenpo( userChoise ){
+    const random = Math.floor( Math.random() * 3 )
+
+    let computerChoise = ''
+
+    switch ( random ) {
+      case 0: computerChoise = 'pedra'; break
+      case 1: computerChoise = 'papel'; break
+      case 2: computerChoise = 'tesoura'; break
+    }
+
+    let result = ''
+
+    if ( computerChoise === 'pedra' ){
+      if ( userChoise === 'pedra' ) result = 'Empate'
+      if ( userChoise === 'papel' ) result = 'Usuario ganhou'
+      if ( userChoise === 'tesoura' ) result = 'Computador ganhou'
+    }
+
+    if ( computerChoise === 'papel' ){
+      if ( userChoise === 'pedra' ) result = 'Computador ganhou'
+      if ( userChoise === 'papel' ) result = 'Empate'
+      if ( userChoise === 'tesoura' ) result = 'Usuario ganhou'
+    }
+
+    if ( computerChoise === 'tesoura' ){
+      if ( userChoise === 'pedra' ) result = 'Usuario ganhou'
+      if ( userChoise === 'papel' ) result = 'Computador ganhou'
+      if ( userChoise === 'tesoura' ) result = 'Empate'
+    }
+
+    this.setState({
+      ...this.state,
+      userChoise: userChoise,
+      computerChoise: computerChoise,
+      result: result
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <StatusBar
+         backgroundColor={ '#2196F3' }
+         barStyle="light-content" />
+
+        <Topo></Topo>
+
+        <View style={ styles.actionPanel }>
+          <View><Button style={ styles.choiseButton } title="Pedra" onPress={ () => this.jokenpo('pedra') } /></View>
+          <View><Button style={ styles.choiseButton } title="Papel" onPress={ () => this.jokenpo('papel') } /></View>
+          <View><Button style={ styles.choiseButton } title="Tesoura" onPress={ () => this.jokenpo('tesoura') } /></View>
+        </View>
+
+        <View style={ styles.stage }>
+          <Text>Escolha do computador: { this.state.computerChoise == '' ? 'Nenhuma escolha' : this.state.computerChoise }</Text>
+          <Text>Escolha do usuario: { this.state.userChoise == '' ? 'Nenhuma escolha' : this.state.userChoise }</Text>
+          <Text style={ styles.resultText }>{ this.state.result }</Text>
+        </View>
       </View>
     );
   }
 }
 
+export class Topo extends Component {
+  render(){
+    return (
+      <View>
+        <Image style={ styles.headerImage } source={ require('./images/jokenpo.png') } />
+      </View>
+    )
+  }
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+  headerImage: {
+    width: '100%'
+  },
+  choiseButton: {
+    width: 90
+  },
+  actionPanel: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 20,
+    marginHorizontal: 20
+  },
+  stage: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginVertical: 20
   },
-  welcome: {
+  resultText: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    color: 'red',
+    marginTop: 10,
+    color: '#2196F3'
+  }
+})
 
 AppRegistry.registerComponent('jokenpo', () => jokenpo);
